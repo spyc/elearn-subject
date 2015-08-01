3b3408b95bd0d9b9d7935bc32d843c355b98eaa8
Tony Yip <tony@opensource.hk>
2015-08-01 15:16:30 +0800

    Migrate from spyc/elearn

---
+++ src/Provider/SubjectServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * elearn
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Tony Yip 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category Guardian
+ * @author   Tony Yip <tony@opensource.hk>
+ * @license  http://opensource.org/licenses/GPL-3.0 GNU General Public License
+ */
+
+namespace Elearn\Subject\Provider;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class SubjectServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if (!$this->app->routesAreCached()) {
+            require __DIR__ . '/routes.php';
+        }
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'subject');
+        $this->publishes([
+            __DIR__ . '/../resources/views' =>  base_path('resources/views/vendor/subject')
+        ]);
+    }
+
+    public function register()
+    {
+    }
+
+}
