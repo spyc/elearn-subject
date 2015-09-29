8c16f1c322ebcb9624ba2f010f7fe19dffb0b158
Tony Yip <tony@opensource.hk>
2015-09-29 22:08:09 +0800

    Change navbar

--- src/resources/views/template/maths.blade.php
+++ src/resources/views/template/maths.blade.php
@@ -17,18 +17,27 @@
                     <li><a href="{{ route('subject.maths.about') }}">About Us</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ route('bug.index') }}">Bug Report</a></li>
                     @if(Auth::check())
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="{{ route('auth.logout') }}">Logout</a></li>
+                                <li>
+                                    <a href="{{ route('auth.logout') }}">
+                                        <i class="fa fa-fw fa-sign-out"></i>
+                                        {{ trans('navbar.logout') }}
+                                    </a>
+                                </li>
                             </ul>
                         </li>
                     @else
-                        <li><a href="{{ route('auth.login') }}">Login</a></li>
+                        <li>
+                            <a href="{{ route('auth.login') }}">
+                                <i class="fa fa-sign-in fa-fw"></i>
+                                {{ trans('navbar.login') }}
+                            </a>
+                        </li>
                     @endif
                 </ul>
             </div>
