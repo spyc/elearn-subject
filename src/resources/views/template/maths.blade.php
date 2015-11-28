e5f2e28448b3ae9a39faf46ebab11716e2d49810
Tony Yip <tony@opensource.hk>
2015-11-28 21:25:42 +0800

    Update for table structure change

--- src/resources/views/template/maths.blade.php
+++ src/resources/views/template/maths.blade.php
@@ -20,7 +20,7 @@
                     @if(Auth::check())
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::user()->ename }} <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
