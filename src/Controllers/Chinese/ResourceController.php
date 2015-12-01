8175377a3aa4310dd62b5c9f52ad3f451d1bc1fc
Tony Yip <tony@opensource.hk>
2015-12-01 22:53:31 +0800

    Add Resource Controller

---
+++ src/Controllers/Chinese/ResourceController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * subject
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Tony Yip 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category Guardian
+ * @author   Tony Yip <tony@opensource.hk>
+ * @license  http://opensource.org/licenses/GPL-3.0 GNU General Public License
+ */
+
+namespace Elearn\Subject\Controllers\Chinese;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class ResourceController extends Controller
+{
+    public function links(Request $request)
+    {
+        $request->getSession()->set('locale', 'zh');
+        return view('subject::chinese.links');
+    }
+}
---
+++ src/resources/views/chinese/links.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+
+@section('wrap')
+    <div class="container">
+        <div class="list-group">
+            <h3>文言文語譯</h3>
+            <a href="{{ url('') }}" class="list-group-item">
+                呂氏春秋
+            </a>
+            <a href="" class="list-group-item">
+                布肆與農人
+            </a>
+        </div>
+    </div>
+@endsection
